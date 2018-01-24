cda99481e760cf61d611cf6f52d10f769fe91691
nakashdavid <nakashdavid@gmail.com>
2018-01-24 02:55:04 +0200

    update

--- cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
@@ -4,20 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/CommandManager.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/CommandManager.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/Controller.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Controller.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/EndCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/EndCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/JoinCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/JoinCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/ListGamesCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/ListGamesCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/NoMoveCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/NoMoveCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/PlayCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/PlayCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/Server.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Server.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/StartCommand.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/StartCommand.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/Task.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Task.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/ThreadPool.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/ThreadPool.cpp.o"
-  "/home/omerz/CLionProjects/c++/ex7/Reversi/src/server/main.cpp" "/home/omerz/CLionProjects/c++/ex7/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/main.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/CommandManager.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/CommandManager.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/Controller.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Controller.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/EndCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/EndCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/JoinCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/JoinCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/ListGamesCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/ListGamesCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/NoMoveCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/NoMoveCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/PlayCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/PlayCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/Server.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Server.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/StartCommand.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/StartCommand.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/Task.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/Task.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/ThreadPool.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/ThreadPool.cpp.o"
+  "/Users/david/CLionProjects/Reversi/src/server/main.cpp" "/Users/david/CLionProjects/Reversi/cmake-build-debug/CMakeFiles/Server.dir/src/server/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
